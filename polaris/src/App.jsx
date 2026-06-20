11f51584163a8dd4f3c27ef09122c9a72222d57b
dasadishree <dasadishree@gmail.com>
2026-06-19 23:20:26 -0400

    weird react steup

---
+++ polaris/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return(
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundImage: "url('/bg.PNG')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+    </div>
+  );
+}
+
+export default App;
